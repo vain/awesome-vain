4d86ea91f3eddbf0f68d292ba38bf731f6c4ab57
Peter Hofmann <scm@uninformativ.de>
2011-08-20 10:57:25 +0200

    layout: uselessfair: Single useless_gap between windows.

--- vain/layout/uselessfair.lua
+++ vain/layout/uselessfair.lua
@@ -35,6 +35,10 @@ local function fair(p, orientation)
         local strip = 0
         for k, c in ipairs(cls) do
             local g = {}
+            -- Save actual grid index for use in the useless_gap
+            -- routine.
+            local this_x = 0
+            local this_y = 0
             if ( orientation == "east" and #cls > 2 )
             or ( orientation == "south" and #cls <= 2 ) then
                 if #cls < (strips * cells) and strip == strips - 1 then
@@ -43,6 +47,9 @@ local function fair(p, orientation)
                     g.width = wa.width / cells
                 end
                 g.height = wa.height / strips
+
+                this_x = cell
+                this_y = strip
 
                 g.x = wa.x + cell * g.width
                 g.y = wa.y + strip * g.height
@@ -55,6 +62,9 @@ local function fair(p, orientation)
                 end
                 g.width = wa.width / strips
 
+                this_x = strip
+                this_y = cell
+
                 g.x = wa.x + strip * g.width
                 g.y = wa.y + cell * g.height
             end
@@ -62,10 +72,24 @@ local function fair(p, orientation)
             -- Useless gap.
             if useless_gap > 0
             then
-                g.width = g.width - 2 * useless_gap
-                g.height = g.height - 2 * useless_gap
-                g.x = g.x + useless_gap
-                g.y = g.y + useless_gap
+                -- Top and left clients are shrinked by two steps and
+                -- get moved away from the border. Other clients just
+                -- get shrinked in one direction.
+                if this_x == 0
+                then
+                    g.width = g.width - 2 * useless_gap
+                    g.x = g.x + useless_gap
+                else
+                    g.width = g.width - useless_gap
+                end
+
+                if this_y == 0
+                then
+                    g.height = g.height - 2 * useless_gap
+                    g.y = g.y + useless_gap
+                else
+                    g.height = g.height - useless_gap
+                end
             end
             -- End of useless gap.
 
